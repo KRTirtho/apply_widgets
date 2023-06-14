bf2b11d8832041b6d4c09747f839f8c7fc86382a
Kingkor Roy Tirtho <krtirtho@gmail.com>
2023-06-14 11:49:43 +0600

    feat: add example

---
+++ example/lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:apply_widgets/apply_widgets.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: const Text('Hello World!').apply(
+          (s) => s
+              .text(const TextStyle(color: Colors.blue))
+              .p(20)
+              .bgColor(Colors.red)
+              .radius(BorderRadius.circular(10)),
+        ),
+      ),
+    );
+  }
+}
